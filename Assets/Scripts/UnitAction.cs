fb66fb6f1fd3834b0fa182b883d51448c23a56cd
vitnemikin <vit.nemikin@yandex.ru>
2023-07-31 17:42:15 +0300

    UnitAction object added to Blue and Red prefabs

--- Assets/Scripts/UnitAction.cs
+++ Assets/Scripts/UnitAction.cs
@@ -1,24 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-namespace UnitAction {
-    public class UnitAction {
-        public enum Types {
-            NONE,
-            RUN,
-            THROW,
-            KICK,
-            JUMP
-        }
+public class UnitAction : MonoBehaviour {
+    public enum Types {
+        NONE,
+        RUN,
+        THROW,
+        KICK,
+        JUMP
+    }
 
-        public Types type;
-        public uint force;
-        public double direction;
-        public double verticalAngle;
+    public Types type = Types.NONE;
+    public uint force = 0;
+    public double direction = 0;
+    public double verticalAngle = 0;
 
-        public UnitAction(Types actionType, uint actionForce, double relativeDirection=0, double verticalAngle=0) {
-            type = actionType;
-            force = (actionForce <= 100) ? actionForce : 100;
-            direction = relativeDirection;
-            this.verticalAngle = verticalAngle;
-        }
+
+    void set(Types actionType, uint actionForce, double relativeDirection = 0, double verticalAngle = 0) {
+        type = actionType;
+        force = (actionForce <= 100) ? actionForce : 100;
+        direction = relativeDirection;
+        this.verticalAngle = verticalAngle;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
